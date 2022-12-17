0721a1745fda53785d10e1a7589a72aae97646c8
marcutudor79 <marcutudor79@gmail.com>
2022-12-17 15:39:23 +0200

    yes

---
+++ build/CMakeFiles/accelerometer.dir/home/bl4ck/pico/pico-sdk/src/rp2_common/pico_stdlib/stdlib.c.obj.d
@@ -0,0 +1,57 @@
+CMakeFiles/accelerometer.dir/home/bl4ck/pico/pico-sdk/src/rp2_common/pico_stdlib/stdlib.c.obj: \
+ /home/bl4ck/pico/pico-sdk/src/rp2_common/pico_stdlib/stdlib.c \
+ /home/bl4ck/pico/pico-sdk/src/common/pico_stdlib/include/pico/stdlib.h \
+ /home/bl4ck/pico/pico-sdk/src/common/pico_base/include/pico.h \
+ /home/bl4ck/pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/bl4ck/pico/pico-sdk/src/common/pico_base/include/pico/assert.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ /usr/include/newlib/assert.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /home/bl4ck/pico/rear_sensor_array/build/generated/pico_base/pico/version.h \
+ /home/bl4ck/pico/pico-sdk/src/common/pico_base/include/pico/config.h \
+ /home/bl4ck/pico/rear_sensor_array/build/generated/pico_base/pico/config_autogen.h \
+ /home/bl4ck/pico/pico-sdk/src/boards/include/boards/pico.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2_common/pico_platform/include/pico/platform.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/platform_defs.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/addressmap.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/sio.h \
+ /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /home/bl4ck/pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/bl4ck/pico/pico-sdk/src/common/pico_base/include/pico/error.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2_common/pico_stdio/include/pico/stdio.h \
+ /home/bl4ck/pico/pico-sdk/src/common/pico_time/include/pico/time.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2_common/hardware_timer/include/hardware/timer.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/timer.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2_common/hardware_base/include/hardware/address_mapped.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/timer.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2_common/hardware_gpio/include/hardware/gpio.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/sio.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/interp.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/padsbank0.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/pads_bank0.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/iobank0.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/io_bank0.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2_common/hardware_irq/include/hardware/irq.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/intctrl.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/m0plus.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2_common/hardware_uart/include/hardware/uart.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/uart.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/uart.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/dreq.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2_common/pico_stdio_usb/include/pico/stdio_usb.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2_common/hardware_pll/include/hardware/pll.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/pll.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/pll.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2_common/hardware_clocks/include/hardware/clocks.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/clocks.h \
+ /home/bl4ck/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/clocks.h \
+ /home/bl4ck/pico/pico-sdk/src/common/pico_binary_info/include/pico/binary_info.h \
+ /home/bl4ck/pico/pico-sdk/src/common/pico_binary_info/include/pico/binary_info/defs.h \
+ /home/bl4ck/pico/pico-sdk/src/common/pico_binary_info/include/pico/binary_info/structure.h \
+ /home/bl4ck/pico/pico-sdk/src/common/pico_binary_info/include/pico/binary_info/code.h
